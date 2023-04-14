3dafb8ee5b00b78357753919449fd50e293b2a1f
Osmar de Oliveira Braz Junior <osmarbraz@hotmail.com>
2023-04-14 11:20:22 -0300

    Início do projeto

--- src/main/java/Principal.java
+++ src/main/java/Principal.java
@@ -10,7 +10,7 @@ import javax.xml.ws.Endpoint;
  */
 public class Principal {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) {        
         Endpoint.publish("http://localhost:8080/CalculadoraServico", new CalculadoraServicoImpl());
         System.out.println("Servico publicado em http://localhost:8080/CalculadoraServico?wsdl");
     }
